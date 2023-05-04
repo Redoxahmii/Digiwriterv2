7664f4a1ce04b60a9813c7a12132884aa2180937
Redoxahmii <fullmetal2334@gmail.com>
2023-05-05 04:37:34 +0500

    unnecessary removal

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes, useLocation } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Navbar from './components/Navbar'
@@ -6,19 +6,14 @@ import Signup from './pages/Signup'
 import Dashboard from './pages/Dashboard/Dashboard'
 import Welcome from './pages/Dashboard/Welcome'
 const App = () => {
-  const location = useLocation()
-  // Hide the Navbar component on the dashboard route
-  // const hideNavbar = location.pathname.startsWith('/dashboard')
   return (
     <>
-      {/* {!hideNavbar && <Navbar />} */}
       <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} >
-          {/* <Route path="/" element={<Home />} /> */}
           <Route path="/dashboard/welcome" element={<Welcome />} />
           <Route path="/dashboard/signup" element={<Signup />} />
         </Route>
