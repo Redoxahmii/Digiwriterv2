029b8f1fef9923668b68171c9e3a5d330727df17
Redoxahmii <fullmetal2334@gmail.com>
2023-05-05 04:37:09 +0500

    unnecessary code removal

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,6 @@ const Navbar = () => {
         if (user) {
             setShowModal(!showModal);
         } else {
-            // Redirect to login page
             () => { navigate('/login') }
         }
     }
