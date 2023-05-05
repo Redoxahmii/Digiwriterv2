3329ef7c0c8b8e3a5fde696636222e2db89673c1
Redoxahmii <fullmetal2334@gmail.com>
2023-05-06 04:57:07 +0500

    editor working

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,23 +9,8 @@ export default {
     },
   },
   daisyui: {
-    themes: [
-      "valentine",
-      "business",
-      {
-        mytheme: {
-          primary: "#52cebb",
-          secondary: "#db7d02",
-          accent: "#d637a4",
-          neutral: "#221F37",
-          "base-100": "#3B3B3B",
-          info: "#54A8F2",
-          success: "#76E0C0",
-          warning: "#F2BE21",
-          error: "#F50A49",
-        },
-      },
-    ],
+    themes: ["valentine", "business", "aqua"],
   },
+  // eslint-disable-next-line no-undef
   plugins: [require("daisyui")],
 };
