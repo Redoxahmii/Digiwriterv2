8099c33493095fe96b236dc4605d66ba256cf406
Redoxahmii <fullmetal2334@gmail.com>
2023-05-14 06:48:09 +0500

    Avatar Online and Offline

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -50,18 +50,22 @@ const Navbar = () => {
                     <div className="flex items-center">
                         {/* The button to open modal */}
                         {user ? (
-                            <button htmlFor="my-modal-3" className="btn btn-ghost ml-1 btn-circle" onClick={handleModal}>
-                                <svg aria-hidden="true" className='w-7 h-7' fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" strokeLinecap="round" strokeLinejoin="round"></path>
-                                </svg>
-                            </button>
+                            <div className='avatar online'>
+                                <button htmlFor="my-modal-3" className="btn btn-ghost ml-1 btn-circle" onClick={handleModal}>
+                                    <svg aria-hidden="true" className='w-7 h-7' fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" strokeLinecap="round" strokeLinejoin="round"></path>
+                                    </svg>
+                                </button>
+                            </div>
                         ) : (
-                            <Link to='/login' className="btn btn-ghost btn-circle">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z" />
-                                </svg>
+                            <div className='avatar offline'>
 
-                            </Link>
+                                <Link to='/login' className="btn btn-ghost btn-circle">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z" />
+                                    </svg>
+                                </Link>
+                            </div>
                         )}
 
 
